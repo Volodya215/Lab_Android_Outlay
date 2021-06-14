f9cf0c93e4778e9dc8b8edbed50e58a5b1ca9631
Volodymyr Dolid <vovad4555@gmail.com>
2021-06-14 04:37:39 +0300

    Added the ability to set spending limits

--- app/src/main/java/com/example/lab_android_outlay/EnterDataFragment.kt
+++ app/src/main/java/com/example/lab_android_outlay/EnterDataFragment.kt
@@ -6,12 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.Toast
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.lab_android_outlay.data.model.UserData
 import com.example.lab_android_outlay.databinding.FragmentEnterDataBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -44,15 +41,13 @@ class EnterDataFragment : Fragment() {
         binding.categoryRecycler.adapter = CategoryAdapter(Category.categories)
 
         binding.submit.setOnClickListener {
-            var sum: Long? = binding.number.text.toString().toLong()
-            var position = CategoryAdapter.index
+            val sum: Long? = binding.number.text.toString().toLong()
+            val position = CategoryAdapter.index
             if(sum == null || position == -1)
                 Toast.makeText(this.context, "You need to select category and enter the cost amount", Toast.LENGTH_LONG).show()
             else
             {
                 try {
-                   // getUserFromDB()
-
                     setValueOfCostsInDatabase(position, sum)
                 }
                 catch (e: Exception)
@@ -69,7 +64,7 @@ class EnterDataFragment : Fragment() {
     {
         firebaseUser = FirebaseAuth.getInstance().currentUser
 
-        var category: String = when(position) {
+        val category: String = when(position) {
             0 -> "houseSum"
             1 -> "carSum"
             2 -> "travelSum"
@@ -79,7 +74,7 @@ class EnterDataFragment : Fragment() {
             6 -> "sweetsSum"
             7 -> "clothesSum"
             8 -> "beautySum"
-            9 -> "TVSum"
+            9 -> "tvsum"
             else -> ""
         }
 
--- app/src/main/java/com/example/lab_android_outlay/SetLimitsFragment.kt
+++ app/src/main/java/com/example/lab_android_outlay/SetLimitsFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.lab_android_outlay.databinding.FragmentRegisterBinding
 import com.example.lab_android_outlay.databinding.FragmentSetLimitsBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import java.lang.Exception
 
 
 /**
@@ -22,7 +26,7 @@ class SetLimitsFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
+    private var firebaseUser: FirebaseUser? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,8 +37,51 @@ class SetLimitsFragment : Fragment() {
         binding.categoryRecycler.layoutManager = GridLayoutManager(requireContext(), 2, RecyclerView.HORIZONTAL, false)
         binding.categoryRecycler.adapter = CategoryAdapter(Category.categories)
 
+        binding.submit.setOnClickListener {
+            val limit: Long? = binding.number.text.toString().toLong()
+            val position = CategoryAdapter.index
+            if(limit == null || position == -1)
+                Toast.makeText(this.context, "You need to select category and enter the limit amount", Toast.LENGTH_LONG).show()
+            else
+            {
+                try {
+                    setValueOfLimitsInDatabase(position, limit)
+                }
+                catch (e: Exception)
+                {
+                    Toast.makeText(this.context, e.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
 
         return binding.root
+    }
+
+    private fun setValueOfLimitsInDatabase(position: Int, limit: Long)
+    {
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+
+        val category: String = when(position) {
+            0 -> "houseLimit"
+            1 -> "carLimit"
+            2 -> "travelLimit"
+            3 -> "groceryLimit"
+            4 -> "sportLimit"
+            5 -> "ticketsLimit"
+            6 -> "sweetsLimit"
+            7 -> "clothesLimit"
+            8 -> "beautyLimit"
+            9 -> "tvlimit"
+            else -> ""
+        }
+
+        FirebaseDatabase.getInstance().reference.child("Users")
+            .child(firebaseUser!!.uid).child(category).setValue(limit)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this.context, "Data updated", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 
     override fun onDestroyView() {
