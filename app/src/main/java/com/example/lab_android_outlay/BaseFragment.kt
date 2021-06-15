7e34be55859b64e80346008490cad23ed26a45a1
Volodymyr Dolid <vovad4555@gmail.com>
2021-06-16 01:09:41 +0300

    Added sensor of device temperature

--- app/src/main/java/com/example/lab_android_outlay/BaseFragment.kt
+++ app/src/main/java/com/example/lab_android_outlay/BaseFragment.kt
@@ -1,5 +1,10 @@
 package com.example.lab_android_outlay
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,19 +17,34 @@ import com.example.lab_android_outlay.databinding.FragmentBaseBinding
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class BaseFragment : Fragment() {
+class BaseFragment : Fragment(), SensorEventListener {
 
     private var _binding: FragmentBaseBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var sensorManager: SensorManager
+    private lateinit var tempSensor: Sensor
+    private var isTempSensorAvailable: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentBaseBinding.inflate(inflater, container, false)
+        sensorManager = context?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) != null)
+        {
+            tempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+            isTempSensorAvailable = true
+        }
+        else
+        {
+            binding.temperature.text = "Temperature sensor is not available"
+            isTempSensorAvailable = false
+        }
 
         binding.showHistoryButton.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
@@ -38,7 +58,7 @@ class BaseFragment : Fragment() {
         }
 
         binding.logOutButton.setOnClickListener {
-            var loginData: LoginDataSource = LoginDataSource()
+            val loginData: LoginDataSource = LoginDataSource()
             loginData.logout()
             findNavController().navigate(R.id.action_FirstFragment_to_loginFragment)
         }
@@ -53,5 +73,27 @@ class BaseFragment : Fragment() {
         _binding = null
     }
 
+    override fun onSensorChanged(event: SensorEvent?) {
+        binding.temperature.text = event!!.values[0].toString() + "°C"
+    }
 
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(isTempSensorAvailable)
+        {
+            sensorManager.registerListener(this, tempSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if(isTempSensorAvailable)
+        {
+            sensorManager.unregisterListener(this)
+        }
+    }
 }
